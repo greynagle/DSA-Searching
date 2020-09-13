3576202129618588f8cb6fe96d029ea0a67d0d18
Grey Nagle <greynagle@gmail.com>
2020-09-13 19:46:09 -0400

    second

--- index.js
+++ index.js
@@ -1,22 +1,55 @@
-function binarySearch(array, value, start, end) {
-    var start = start === undefined ? 0 : start;
-    var end = end === undefined ? array.length : end;
+// assuming the items of the library are in the type
+// [..., ..., {title:'Sample', DDS:'123.45'}]
+
+function deweySearch(systemArr, value, title, start, end) {
+    let start = start === undefined ? 0 : start;
+    let end = end === undefined ? systemArr.length : end;
 
     if (start > end) {
         return -1;
     }
 
     const index = Math.floor((start + end) / 2);
-    const item = array[index];
+    const item = systemArr[index].DDS;
 
     console.log(start, end);
     if (item == value) {
-        return index;
+        // checks down from landing until it finds the result
+		let result = linearSearch(systemArr, index, false, value, title);
+        if (result !== -1) {
+            return result;
+        } else {
+			// if fail, check up from landing
+            result = linearSearch(systemArr, index, true, value, title);
+            if (result !== -1) {
+                return result;
+            } else {
+				// if double fail, throw error
+				throw new Error('Result not found')
+			}
+        }
     } else if (item < value) {
-		console.log(index)
-        return binarySearch(array, value, index + 1, end);
+        return binarySearch(systemArr, value, title, index + 1, end);
     } else if (item > value) {
-		console.log(index)
-        return binarySearch(array, value, start, index - 1);
+        return binarySearch(systemArr, value, title, start, index - 1);
     }
 }
+
+// because multiple books can be in one decimal value, need to check
+// titles once you land in the range. This is basically indexOf, but 
+// starting in the middle of the array and kicking back a flag to
+// switch directions when necessary
+function linearSearch(array, index, direction, value, title) {
+    if (array[index].DDS != value) {
+        return -1;
+    }
+    if (array[index].title === title) {
+        return index;
+    } else {
+        let newIndex = direction ? index + 1 : index - 1;
+        return linearSearch(array, newIndex, direction, value, title);
+    }
+}
+
+
+
